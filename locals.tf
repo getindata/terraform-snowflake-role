da936972cba963b3a8c8b93257710c784e794c44
Jakub Igła <jakub.igla@getindata.com>
2023-01-03 13:54:38 +0100

    fix: Only build name if module enabled (#3)

--- locals.tf
+++ locals.tf
@@ -1,9 +1,9 @@
 locals {
   # Get a name from the descriptor. If not available, use default naming convention.
   # Trim and replace function are used to avoid bare delimiters on both ends of the name and situation of adjacent delimiters.
-  name_from_descriptor = trim(replace(
+  name_from_descriptor = module.role_label.enabled ? trim(replace(
     lookup(module.role_label.descriptors, var.descriptor_name, module.role_label.id), "/${module.role_label.delimiter}${module.role_label.delimiter}+/", module.role_label.delimiter
-  ), module.role_label.delimiter)
+  ), module.role_label.delimiter) : null
 
   granted_roles    = compact(var.granted_roles)
   granted_to_roles = compact(var.granted_to_roles)
