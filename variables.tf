8870cd6cbcd748bd69e6980f91f207d33081666e
Piotr Sierkin <piotr.sierkin@getindata.com>
2023-07-11 09:58:13 +0200

    chore: revert changes

--- variables.tf
+++ variables.tf
@@ -83,6 +83,7 @@ variable "view_grants" {
     database_name = string
     schema_name   = string
     view_name     = optional(string)
+    on_future     = optional(bool, false)
     privileges    = list(string)
   }))
   default = []
